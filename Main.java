38f5831048950098661b424ce6e9ead02d8f2deb
alecmr2000 <alecmr2000@gmail.com>
2021-01-19 18:10:14 +0000

    Just added comments to comply with week two class.

--- Main.java
+++ Main.java
@@ -3,4 +3,7 @@ class Main {
     System.out.println("Hello world!");
     System.out.println("My name is Alec");
   }
+  /*This is how to do a comment in Java.
+  You can make the comments multiple lines. */
+  //Of course, you still have the single line comments.
 }
